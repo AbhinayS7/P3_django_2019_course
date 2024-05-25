373cdf62dc30872ebdc681dcaef8629345eb20e3
AbhinayS7 <abhinay1997soni@gmail.com>
2024-05-25 15:47:06 +0530

    video 78 completed, handling post request on submitting contact us form and saving data to models/db

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 def home(request): 
@@ -6,6 +7,13 @@ def home(request):
     # return HttpResponse('This is home ')
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content =request.POST['content']
+        contact=Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
     return render(request,'home/contact.html')
     # return HttpResponse("This is contact")
 
